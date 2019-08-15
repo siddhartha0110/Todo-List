ac324574b75df9829f6859a878ce3259ed755e38
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2019-08-16 00:48:55 +0530

    Basic Todo List

---
+++ list.js
@@ -0,0 +1,22 @@
+$("ul").on("click","li",function(){
+    $(this).toggleClass("finished");
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which===13){
+        var todo=$(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span>"+todo+"</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
